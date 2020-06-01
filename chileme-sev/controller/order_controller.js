f14347ed3669c6f00404efa00663d2d3e594ca6e
917501958 <917501958@qq.com>
2020-06-01 16:34:03 +0800

    上传

--- chileme-sev/controller/order_controller.js
+++ chileme-sev/controller/order_controller.js
@@ -1,7 +1,21 @@
 const Order = require('../model/order_schema')
-
+const Goods = require('../model/goods_schema')
 // 新增订单
 const addOrder = async ctx => {
+    let req = ctx.request.body
+    console.log(req)
+    let collectGoods = []
+    await Goods.find({goodsId:{$in:req.ids}})
+    .then(res => {
+        console.log(res)
+        collectGoods = res
+    }).catch(err => {
+        console.log(err)
+        ctx.body = {
+            success : false,
+            msg:'查询商品数据失败'
+        }
+    })
     ctx.body='新增成功'
 }
 // 查看订单
